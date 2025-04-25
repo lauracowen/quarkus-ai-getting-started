0100b9914e1b5a531af56c2e07fbd0569731c52c
Laura Cowen <laura_cowen@uk.ibm.com>
2025-04-25 19:57:50 +0100

    Created clean project from Quarkus Starter. Added explicit annotation to each of the REST method's parameters as there is more than one.

--- src/main/java/org/acme/Poems.java
+++ src/main/java/org/acme/Poems.java
@@ -1,8 +1,9 @@
-package io.quarkus;
+package org.acme;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -15,7 +16,7 @@ public class Poems {
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/{mytopic}/{lines}")  //<2>
-    public String showMeAPoem(String mytopic, int lines) {  //<3>
+    public String showMeAPoem(@PathParam("topic") String mytopic, @PathParam("lines") int lines) {  //<3>
         return aiPoemService.writeAPoem(mytopic, lines);  // <4>
     }
 }
